fd6153e6e7000d3101c1f791fdadde98570e8421
skaltman <sara.altman@posit.co>
2026-04-28 16:13:54 -0700

    Built site for gh-pages

--- website/_extensions/gadenbuie/tachyons/tachyons.lua
+++
@@ -1,9 +0,0 @@
-function Pandoc()
-  if quarto.doc.is_format("html:js") then
-    quarto.doc.add_html_dependency({
-      name = 'tachyons',
-      version = '4.12.0',
-      stylesheets = { "tachyons.css", "quarto-resets.css" },
-    })
-  end
-end
